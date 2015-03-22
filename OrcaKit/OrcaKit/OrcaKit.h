726aa4833bcedbc7c31de6ec0e7e884df4a2d5f5
Chunlea Ju <ichunlea@me.com>
2015-03-23 05:57:40 +0800

    Add OrcaKit project, which is the framework.

---
+++ OrcaKit/OrcaKit/OrcaKit.h
@@ -0,0 +1,19 @@
+//
+//  OrcaKit.h
+//  OrcaKit
+//
+//  Created by Chunlea Ju on 3/23/15.
+//  Copyright (c) 2015 Chunlea Ju. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OrcaKit.
+FOUNDATION_EXPORT double OrcaKitVersionNumber;
+
+//! Project version string for OrcaKit.
+FOUNDATION_EXPORT const unsigned char OrcaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrcaKit/PublicHeader.h>
+
+
